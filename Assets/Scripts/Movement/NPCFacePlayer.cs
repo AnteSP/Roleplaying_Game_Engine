38dd4bba305b651bd41a0c732ade3f33d8395212
Ante Seifert-Pupic <aseifert-pupic@mantech-inc.com>
2025-02-17 23:29:10 -0500

    Added Mefisto

---
+++ Assets/Scripts/Movement/NPCFacePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NPCFacePlayer : NPCMovement
+{
+    public bool FacePlayer = true;
+
+
+    private void Update()
+    {
+        if (FacePlayer) Face(Stats.current.Player.transform.position);
+    }
+}
